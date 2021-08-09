521a5159cfdfb9c8dc80e9413aacddceadbae37e
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-09 13:42:37 -0600

    Remove OpenAPI Config. Using OpenAPIDefinitation annotation instead.

--- microservice-template/src/main/java/com/aline/microservicetemplate/MicroserviceTemplateApplication.java
+++ microservice-template/src/main/java/com/aline/microservicetemplate/MicroserviceTemplateApplication.java
@@ -1,9 +1,18 @@
 package com.aline.microservicetemplate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+    @Info(
+            title = "Microservice Template",
+            description = "(Update this description) This is a description of your microservice and it's functionality.",
+            version = "1.0"
+    )
+)
 public class MicroserviceTemplateApplication {
 
     public static void main(String[] args) {
