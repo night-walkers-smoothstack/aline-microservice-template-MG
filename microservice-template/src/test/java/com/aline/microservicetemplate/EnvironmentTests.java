540b117f94bd7c3df7cd03bb4cf5794ac4157133
leandroyabut <leandro.yabut@smoothstack.com>
2021-08-28 18:38:29 -0600

    Add default test properties.

---
+++ microservice-template/src/test/java/com/aline/microservicetemplate/EnvironmentTests.java
@@ -0,0 +1,12 @@
+package com.aline.microservicetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
+public class EnvironmentTests {
+    @Test
+    void test_correctVersion() {
+
+    }
+}
